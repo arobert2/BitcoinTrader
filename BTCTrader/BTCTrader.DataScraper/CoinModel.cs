2987e2d3b56fae6c8e50e756b6bac7a94dbc19ea
Allen Roberts <allen.roberts1985@gmail.com>
2018-08-05 02:52:10 -0400

    Initial commit

---
+++ BTCTrader/BTCTrader.DataScraper/CoinModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTCTrader.DataScraper
+{
+    public class CoinModel
+    {
+        public DateTime Timestamp { get; set; }
+        public bool Sucess { get; set; }
+        public string Message { get; set; }
+        public CoinPrice Price { get; set; }
+
+        public CoinModel()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+
+    public class CoinPrice
+    {
+        public double Bid { get; set; }
+        public double Ask { get; set; }
+        public double Last { get; set; }
+    }
+}
---
+++ BTCTrader/BTCTrader.DataScraper/MarketScraper.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTCTrader.DataScraper
+{
+    public class MarketScraper
+    {
+        public string Market { get; set; }
+
+        private HttpClient HttpClient { get; set; } = new HttpClient();
+        /// <summary>
+        /// Scrape the market
+        /// </summary>
+        /// <returns>Coin model of market</returns>
+        public async Task<CoinModel> ScrapeMarket()
+        {
+            CoinModel cm = null;
+            HttpResponseMessage resp = await HttpClient.GetAsync("https://bittrex.com/api/v1.1/public/getticker?market=" + Market);
+            if (resp.IsSuccessStatusCode)
+            {
+                string r = await resp.Content.ReadAsStringAsync();
+                cm = JsonConvert.DeserializeObject<CoinModel>(r);
+            }
+            return cm;
+        }
+    }
+}
